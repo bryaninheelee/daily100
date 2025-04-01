a2a9e2f60bf92c50098404a5779d8ac1b65687fe
bryaninheelee <bryaninheelee@gmail.com>
2025-04-01 21:41:47 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 }
